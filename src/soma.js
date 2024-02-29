3946a3bde1db1dc91f490cbcd17f8ac761133efd
diogoscastro <diogocas89@gmail.com>
2024-02-29 12:15:04 -0300

    first commit

---
+++ src/soma.js
@@ -0,0 +1,8 @@
+function soma(a, b) {
+    return a + b;
+  }
+  
+  const resultado = soma(5, 10);
+console.log(`Resultado da soma: ${resultado}`); // Resultado: 15
+
+module.exports = soma;
---
+++ src/soma.test.js
@@ -0,0 +1,21 @@
+// Importa a função soma
+const soma = require('./soma');
+
+// Descreve a função a ser testada
+describe('Função Soma', () => {
+
+  // Testa se a função retorna a soma de dois números
+  it('deve retornar a soma de dois números', () => {
+    const resultado = soma(5, 10);
+    // Usa o matcher `toBe` para verificar se o resultado é igual ao valor esperado
+    expect(resultado).toBe(15);
+  });
+
+
+  // Testa se a função retorna 0 para a soma de 0 e 0
+  it('deve retornar 0 para a soma de 0 e 0', () => {
+    const resultado = soma(0, 0);
+    expect(resultado).toBe(0);
+  });
+});
+
